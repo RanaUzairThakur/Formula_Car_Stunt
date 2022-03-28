4aa65f4e437218fb8e8025e8d0409d42cbed4d6c
RanaUzairThakur <66404667+RanaUzairThakur@users.noreply.github.com>
2022-03-28 01:41:52 -0700

    First Push assets

---
+++ Assets/3rdPartyPlugin/CNControls/Examples/Scenes/Touchpad Camera/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using CnControls;
+using UnityEngine;
+
+namespace Examples.Scenes.TouchpadCamera
+{
+    public class RotateCamera : MonoBehaviour
+    {
+        public float RotationSpeed = 15f;
+        public float speed;
+        public Transform OriginTransform;
+
+        public void Update()
+        {
+            OriginTransform.Rotate(Vector3.up * speed* Time.deltaTime);
+            var horizontalMovement = CnInputManager.GetAxis("Horizontal");
+
+            OriginTransform.Rotate(Vector3.up, horizontalMovement * Time.deltaTime * RotationSpeed);
+        }
+    }
+}
